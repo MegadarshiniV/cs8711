237d697898db64f323e16ea47edaf631168e55c1
MegadarshiniV <142490085+MegadarshiniV@users.noreply.github.com>
2023-08-17 16:18:40 +0530

    Update main.py

--- Cs8711/main.py
+++ Cs8711/main.py
@@ -1,1 +1,19 @@
-print("Hello World")
+from flask import Flask, request, render_template
+import requests
+app=Flask(__name__)
+@app.route('/',methods=["GET","POST"])
+def index():
+  weatherData=''
+  error=0
+  cityName=''
+  if request.method=="POST":
+    cname=request.form.get("cname")
+    if cname:
+      weatherAPIKey= '0cdf9b5c30bf1e5197a1703f20b5d14d'
+      url="https://api.openweathermap.org/data/2.5/weather?q="+cname+"&appid=" + weatherApiKey
+            weatherData = requests.get(url).json()
+    else:
+      error = 1
+  return render_template('index.html', data = weatherData, cityName = cityName, error = error)
+if __name__ == "__main__":
+    app.run(host='127.0.0.1',port=8000,debug=True)
